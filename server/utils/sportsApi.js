ab4d7cbba633c8a4619624a836b728e5230d7723
boy-son <branden.wheat@yahoo.com>
2023-03-10 11:10:18 -0500

    added api routes for da big games

--- server/utils/sportsApi.js
+++ server/utils/sportsApi.js
@@ -1,15 +1,119 @@
+const API_URL = 'https://odds.p.rapidapi.com/v4/sports';
 var keyArr = ['2ca9e05eb2mshaad55f69b7a7e24p152fdajsn1f4c0544ad64',
   'c1a48b6beemsh933a4cb8cf03335p126b55jsnd037988b766f', '92892adfc3msh6c3519920a0dd57p1c0fcbjsn891d97cf349d', 'eaaa813926msh7bc3448e9439517p1e2fffjsn1e58d3a059e7']
 
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': keyArr[1],
-		'X-RapidAPI-Host': 'odds.p.rapidapi.com'
-	}
-};
-
-fetch('https://odds.p.rapidapi.com/v4/sports?all=true', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+	export async function getSports() {
+		const response = await fetch(API_URL, {
+			method: 'GET',
+			headers: {
+				'x-rapidapi-host': 'odds.p.rapidapi.com',
+				'x-rapidapi-key': keyArr[Math.floor(Math.random() * keyArr.length)]
+			}
+		});
+		const data = await response.json();
+		return data;
+	};
+	
+	
+	export async function getOddsBySport(sport) {
+		const response = await fetch(API_URL + '/' + sport + '/odds', {
+			method: 'GET',
+			headers: {
+				'x-rapidapi-host': 'odds.p.rapidapi.com',
+				'x-rapidapi-key': keyArr[Math.floor(Math.random() * keyArr.length)]
+			}
+		});
+		const data = await response.json();
+		return data;
+	};
+	
+	
+	export async function getOddsByRegion(region) {
+		const response = await fetch(API_URL + '/odds?region=' + region, {
+			method: 'GET',
+			headers: {
+				'x-rapidapi-host': 'odds.p.rapidapi.com',
+				'x-rapidapi-key': keyArr[Math.floor(Math.random() * keyArr.length)]
+			}
+		});
+		const data = await response.json();
+		return data;
+	};
+	
+	
+	export async function getOddsBySportAndRegion(sport, region) {
+		const response = await fetch(API_URL + '/' + sport + 'odds?regions=' + region, {
+			method: 'GET',
+			headers: {
+				'x-rapidapi-host': 'odds.p.rapidapi.com',
+				'x-rapidapi-key': keyArr[Math.floor(Math.random() * keyArr.length)]
+			}
+		});
+		const data = await response.json();
+		return data;
+	};
+	
+	
+	export async function getOddsBySportAndRegionAndMarket(sport, region, market) {
+		const response = await fetch(API_URL + '/' + sport + '/' + 'odds?regions=' + region + '&markets=' + market, {
+			method: 'GET',
+			headers: {
+				'x-rapidapi-host': 'odds.p.rapidapi.com',
+				'x-rapidapi-key': keyArr[Math.floor(Math.random() * keyArr.length)]
+			}
+		});
+		const data = await response.json();
+		return data;
+	};
+	
+	
+	export async function getOddsBySportAndRegionAndMarketAndOddType(sport, region, market, oddsFormat) {
+		const response = await fetch(API_URL + '/' + sport + '/' + 'odds?regions=' + region + '&oddsFormat=' + oddsFormat + '&markets=' + market, {
+			method: 'GET',
+			headers: {
+				'x-rapidapi-host': 'odds.p.rapidapi.com',
+				'x-rapidapi-key': keyArr[Math.floor(Math.random() * keyArr.length)]
+			}
+		});
+		const data = await response.json();
+		return data;
+	};
+	
+	
+	export async function getOddsBySportAndRegionAndMarketAndOddTypeAndDate(sport, region, market, oddsFormat, date) {
+		const response = await fetch(API_URL + '/' + sport + '/' + 'odds?regions=' + region + '&oddsFormat=' + oddsFormat + '&markets=' + market + '&dateFormat=' + date, {
+			method: 'GET',
+			headers: {
+				'x-rapidapi-host': 'odds.p.rapidapi.com',
+				'x-rapidapi-key': keyArr[Math.floor(Math.random() * keyArr.length)]
+			}
+		});
+		const data = await response.json();
+		return data;
+	};
+	
+	
+	export async function  getScoresBySport(sport) {
+		const response = await fetch(API_URL + '/' + sport + '/scores', {
+			method: 'GET',
+			headers: {
+				'x-rapidapi-host': 'odds.p.rapidapi.com',
+				'x-rapidapi-key': keyArr[Math.floor(Math.random() * keyArr.length)]
+			}
+		});
+		const data = await response.json();
+		return data;
+	};
+	
+	
+	export async function getScoresBySportAndDays (sport, days) {
+		const response = await fetch(API_URL + '/' + sport + '/scores?daysFrom=' + days, {
+			method: 'GET',
+			headers: {
+				'x-rapidapi-host': 'odds.p.rapidapi.com',
+				'x-rapidapi-key': keyArr[Math.floor(Math.random() * keyArr.length)]
+			}
+		});
+		const data = await response.json();
+		return data;
+	};
