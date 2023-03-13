4804b2676555d4a58674ea6544a63b7fa4a0acb5
boy-son <branden.wheat@yahoo.com>
2023-03-13 14:26:48 -0400

    added files to components folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,23 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Leaderboards from './pages/Leaderboards';
+import Profile from './pages/Profile';
+import NotFound from './pages/NotFound';
+import Navbar from './components/Navbar';
 
-import React from "react";
-import Home from "./pages/Home";
-
-const App = () => <Home />;
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/leaderboards" component={Leaderboards} />
+        <Route path="/profile" component={Profile} />
+        <Route component={NotFound} />
+      </Switch>
+    </>
+  );
+}
 
 export default App;
